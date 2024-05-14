387e17c9675826a187b454c45179c67618af29b8
Justin Shetty <jas0504@gmail.com>
2024-05-14 16:08:20 +0900

    HDF5 homework

--- 09_io/17_phdf5_read.cpp
+++ 09_io/17_phdf5_read.cpp
@@ -12,6 +12,7 @@ int main (int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   assert(mpisize == dim[0]*dim[1]);
+
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
   hid_t file = H5Fopen("data.h5", H5F_ACC_RDONLY, plist);
@@ -20,15 +21,20 @@ int main (int argc, char** argv) {
   int ndim = H5Sget_simple_extent_ndims(globalspace);
   hsize_t N[ndim];
   H5Sget_simple_extent_dims(globalspace, N, NULL);
+
   hsize_t NX = N[0], NY = N[1];
-  hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
+  hsize_t block[2] = {NX/4, NY/4};
+  hsize_t Nlocal[2] = {2*block[0], 2*block[1]};
   hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
+  for(int i=0; i<2; i++) offset[i] *= block[i];
+  hsize_t count[2] = {2,2};
+  hsize_t stride[2] = {2,2};
+  for(int i=0; i<2; i++) stride[i] *= block[i];
+
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
+
   vector<int> buffer(Nlocal[0]*Nlocal[1]);
   plist = H5Pcreate(H5P_DATASET_XFER);
   H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
@@ -44,7 +50,7 @@ int main (int argc, char** argv) {
   int sum = 0;
   for (int i=0; i<Nlocal[0]*Nlocal[1]; i++)
     sum += buffer[i];
-  printf("sum=%d\n",sum);
+  printf("[rank%d] sum=%d\n",mpirank, sum);
   printf("N=%d: %lf s (%lf GB/s)\n",NX*NY,time,4*NX*NY/time/1e9);
   MPI_Finalize();
 }
